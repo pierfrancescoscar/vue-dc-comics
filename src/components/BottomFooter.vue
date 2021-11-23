486f6eb90c992683cfa632477aea60629d34168a
pierfrancescoscar <scarinci401@gmail.com>
2021-11-23 18:13:32 +0100

    ADD: left bottom footer content

--- src/components/BottomFooter.vue
+++ src/components/BottomFooter.vue
@@ -1,13 +1,16 @@
 <template>
+<div class="bottom-footer p-3">
   <section class="container d-flex">
       <div class="left-bottom-footer w-50">
-          Test
+          <button class="text-uppercase p-2">Sign-up now!</button>
       </div>
 
-      <div class="right-bottom-footer w-50">
-          Test 2
+      <div class="right-bottom-footer w-50 d-flex">
+          <h4 class="text-uppercase">Follow us</h4>
+          
       </div>
   </section>
+</div>
 </template>
 
 <script>
@@ -17,5 +20,12 @@ export default {
 </script>
 
 <style scoped lang="scss">
-
+    .bottom-footer {
+        background-color: #303030;
+    }
+        button {
+            background: transparent;
+            color: white;
+            border: 1px solid #0282F9;
+        }
 </style>
